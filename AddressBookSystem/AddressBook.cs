0cc3495bff0cb588f1b8374e303b7b71295da007
Omkarbade <omakrbade3799@gmail.com>
2022-02-01 14:35:24 +0530

    [Omkar] Add Address book program

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        //properties of AddressBook
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+        public void Contact()  //Method to contact 
+        {
+            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName
+                              + " Address: " + this.Address + " City: " + this.City + " State: "
+                              + this.State + " Zip: " + this.Zip + " PhoneNumber: "
+                              + this.PhoneNumber + " Email: " + this.Email);
+        }
+    }
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBookSystem program");
+
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            AddressBook addressBook = new AddressBook()  //Initializing elements using collection-initializer syntax.
+
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.Contact();
+            Console.ReadLine();
+        }
+    }
+}
+    
